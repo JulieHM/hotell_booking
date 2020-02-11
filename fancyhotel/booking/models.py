6bd02a7ef98d89294e333639bb42298adeee5ad2
Andreas Berg <andrebbe@stud.ntnu.no>
2020-02-11 20:28:23 +0100

    Add models Hotelroom, Customer & Booking to models.py/database
    This commit adds models for Hotelroom, Customer and Bookings to
    fancyhotel/booking/models.py, and migrates them.
    
    Hotelroom
     - (int) floor
     - (int) numberOnFloor [2 digits]
     - (int) singleBeds
     - (int) doubleBeds
     - (int) pricePrNight
     - (bool) includedBreakfast
     - (bool) includedParking
     - (bool) includedCancelling
     - (bool) smokingAllowed
     - (int) numberOfBeds [auto-calculated]
    
    Customer
     - (string) firstName [max 20 digits]
     - (string) lastName [max 40 digits]
     - (Email) email [unique]
     - (string) phoneNr [max 8 digits]
    
    Booking
     - (ForeignKey) room [Hotelroom]
     - (ForeignKey) customerID [Customer, can be NULL]
     - (string) firstName [max 20 digits]
     - (string) lastName [max 40 digits]
     - (Email) email
     - (string) phoneNr [max 8 digits]
     - (Date) dateStart
     - (Date) dateEnd
    
    While I have not yet populated the database, I figured this would
    give us something to work on tomorrow (and in the upcoming sprint)
    
    Related to #2 :)

--- fancyhotel/booking/models.py
+++ fancyhotel/booking/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 # Create your models here.
+
+########################### HOTELROOM ###########################
+class Hotelroom(models.Model):
+    floor = models.IntegerField(default=1)
+    numberOnFloor = models.IntegerField(default=1,
+                                        validators=[MinLengthValidator(2, message="Ensure numberOnFloor has two digits (00-99)"),
+                                                    MaxLengthValidator(2, message="Ensure numberOnFloor has two digits (00-99)")])
+    singleBeds = models.IntegerField(default=0)
+    doubleBeds = models.IntegerField(default=0)
+    pricePrNight = models.IntegerField()
+    includedBreakfast = models.BooleanField(default = True)
+    includedParking = models.BooleanField(default = True)
+    includedCancelling = models.BooleanField(default = False)
+    smokingAllowed = models.BooleanField(default = False)
+
+    @property
+    def numberOfBeds(self):
+        return self.singleBeds + 2 * self.doubleBeds
+
+    class Meta:
+        models.UniqueConstraint(fields=['floor', 'numberOnFloor'], name='unique_room_on_floor')
+
+    def __str__(self):
+        return F"{self.floor}{self.numberOnFloor}"
+
+
+
+########################### CUSTOMER ###########################
+class Customer(models.Model):
+    firstName = models.CharField(max_length=20)
+    lastName = models.CharField(max_length=40)
+    email = models.EmailField(unique=True)
+    phoneNr = models.CharField(max_length=8)
+
+    def __str__(self):
+        return F"{self.lastName},  {self.firstName}"
+
+
+
+########################### BOOKING ###########################
+class Booking(models.Model):
+    room = models.ForeignKey(Hotelroom, on_delete=models.PROTECT)
+    customerID = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    firstName = models.CharField(max_length=20)
+    lastName = models.CharField(max_length=40)
+    email = models.EmailField()
+    phoneNr = models.CharField(max_length=8)
+    dateStart = models.DateField(verbose_name="Start date")
+    dateEnd = models.DateField(verbose_name="End date")
+
+    def __str__(self):
+        return F"Room {self.room}: {self.dateStart} - {self.dateEnd}"
