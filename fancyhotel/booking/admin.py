4362fad7d5c2bd2c0004327fe51415b9cd48afb7
Andreas Berg <andrebbe@stud.ntnu.no>
2020-02-12 11:10:43 +0100

    Add admin-user, and change server/admin to change models
    This commit enables an Admin-user to change/create/delete all
    models currently in booking.models, using Django's built-in Admin-
    system.
    
    Related to #19, #17, #13, #5

--- fancyhotel/booking/admin.py
+++ fancyhotel/booking/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Hotelroom, Customer, Booking
 
 # Register your models here.
+
+admin.site.register(Hotelroom)
+admin.site.register(Customer)
+admin.site.register(Booking)
