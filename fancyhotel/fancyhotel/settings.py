a648bf76e847ff21143ae440204611b16bfe90fa
Andreas Berg <andrebbe@stud.ntnu.no>
2020-02-12 11:22:03 +0100

    Change timezone to "Europe/Oslo"
    This commit changes the timezone, simple as that. That's all.

--- fancyhotel/fancyhotel/settings.py
+++ fancyhotel/fancyhotel/settings.py
@@ -107,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-US'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Oslo'
 
 USE_I18N = True
 
