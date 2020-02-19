cc856d013f133b25b055d2ee59cddd058b1fc28a
Andreas Berg <andrebbe@stud.ntnu.no>
2020-02-19 15:15:08 +0100

    Add ability to search for available Hotelrooms at /booking/room/
    This commit adds the ability to search for available Hotelrooms,
    and will display a (rather ugly) list of free room numbers. Please
    note that the front-end parts are not in any way done yet!
    
    Also adds room/<roomNr>, which is a site for each room. Little to
    no information there as of yet, but it might be a "staging ground"
    for bookings, potentially. Still cool to have, I guess - maybe even
    with a description field for each room?
    
    Related to #14

--- fancyhotel/booking/urls.py
+++ fancyhotel/booking/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    
+    path('room/', views.getRooms, name='search rooms'),
+
+    #eks: /booking/room/101/
+    path('room/<int:roomNr>/', views.room, name='room info'),
+
 ]
